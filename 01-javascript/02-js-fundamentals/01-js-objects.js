89af4fb363ab3c11f02b68a69b09247f3e55ced6
christian torio <christiantorio@aol.com>
2018-11-07 23:59:13 -0800

    adding objects and hoisting assignment

--- 01-javascript/02-js-fundamentals/01-js-objects.js
+++ 01-javascript/02-js-fundamentals/01-js-objects.js
@@ -1,1 +1,41 @@
 // 30 minutes max
+
+let students = [
+    { name: 'Remy', cohort: 'Jan' },
+    { name: 'Genevieve', cohort: 'March' },
+    { name: 'Chuck', cohort: 'Jan' },
+    { name: 'Osmund', cohort: 'June' },
+    { name: 'Nikki', cohort: 'June' },
+    { name: 'Boris', cohort: 'June' }
+];
+
+function displayName(){
+    for (var key in students){
+        console.log('Name:', students[key]['name'], ', Cohort:',students[key]['cohort']);
+    }
+};
+
+
+let users = {
+    employees: [
+        { 'first_name':  'Miguel', 'last_name' : 'Jones' },
+        { 'first_name' : 'Ernie', 'last_name' : 'Bertson' },
+        { 'first_name' : 'Nora', 'last_name' : 'Lu' },
+        { 'first_name' : 'Sally', 'last_name' : 'Barkyoumb' }
+    ],
+    managers: [
+       { 'first_name' : 'Lillian', 'last_name' : 'Chambers' },
+       { 'first_name' : 'Gordon', 'last_name' : 'Poe' }
+    ]
+ };
+
+function displayUsers(){
+    for (var key in users){
+        console.log(key);
+        for (var i = 0; i < users[key].length; i++){
+            console.log(i, '-', users[key][i].last_name,',',users[key][i].first_name,'-',users[key][i].last_name.length + users[key][i].first_name.length);
+        }
+    };
+};
+
+displayUsers(users);
---
+++ 01-javascript/02-js-fundamentals/02-hoisting.js
@@ -0,0 +1,105 @@
+// Challenge 1
+
+// var hello;
+// console.log(hello);
+// hello = 'world';
+
+// produces undefined output
+
+// Challenge 2
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// };
+
+// output is magnet because it is within scope of the function
+
+// Challenge 3
+
+// var brendan = 'super cool';
+// // function print() {
+// brendan = 'only okay';
+// console.log(brendan);
+// }
+// console.log(brendan);
+
+// output is super cool since it is referring to the global var
+
+// Challenge 4
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat() {
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+// will output chicken and half chicken. chicken is is initialized globally. food defines half chicken in the function which will then be shown on the console
+
+// Challenge 5
+
+// var mean = function () {
+//     food = 'chicken';
+//     console.log(food);
+//     var food = 'fish';
+//     console.log(food);
+// }
+
+// mean();
+// console.log(food);
+
+// will output mean is not a function. will need to move calling the mean function below since it is initialized as mean and remove console.log(food) above the function since it is not referencing anything
+
+// Challenge 6
+
+// // console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+// 	genre = "rock";
+// 	console.log(genre);
+// 	var genre = "r&b";
+// 	console.log(genre);
+// }
+// console.log(genre);
+
+// first console.log cannot reference the function below. since genre is a global var it will print on last console.log line. rock and r&b will be shown first
+
+// Challenge 7
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+// 	dojo = "seattle";
+// 	console.log(dojo);
+// 	var dojo = "burbank";
+// 	console.log(dojo);
+// }
+// console.log(dojo);
+
+// san jose will display first then seattle and burbank. since dojo is called again. san jose will show again last
+
+// Challenge 8
+
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+// function makeDojo(name, students){
+//         const dojo = {};
+//         dojo.name = name;
+//         dojo.students = students;
+//         if(dojo.students > 50){
+//             dojo.hiring = true;
+//         }
+//         else if(dojo.students <= 0){
+//             dojo = "closed for now";
+//         }
+//         return dojo;
+// }
+
+// will produce error since dojo array is const. 
